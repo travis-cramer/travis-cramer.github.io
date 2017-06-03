b5f38cb9ef1afbb4bd22d564889ea8cbc63306e5
travis-cramer14 <travis.cramer14@gmail.com>
2017-06-03 16:04:37 -0700

    new colors, new fadeIns

---
+++ projects.js
@@ -0,0 +1,3 @@
+$(document).ready( function() {
+	$("#projects-title").delay(100).fadeTo(1200, 1)
+});
---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#myname").delay(100).fadeTo(1200, 1);
+    $("#mytitle").delay(200).fadeTo(1200, 1);
+    $("#resume").delay(300).fadeTo(1200, 1);
+})
+    
+
+
