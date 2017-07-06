097c555687f43c3353f5279b18286c29e3e96db4
travis-cramer14 <travis.cramer14@gmail.com>
2017-07-06 23:20:49 +0800

    minor tweaks. Changed 'jQuery' to 'Javascript'

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready( function(){
     var titles = ["B.S. Mathematical Sciences", "Student Developer", "Data Engineer", "Avid Reader", "Desperate College-grad"];
     
-    $("#myname").delay(100).fadeTo(1200, 1);
-    $("#mytitle").text(titles[0]).delay(200).fadeTo(1200, 1);
-    $("#resume").delay(300).fadeTo(1200, 1);
+    $("#myname").delay(200).fadeTo(1800, 1);
+    $("#mytitle").text(titles[0]).delay(600).fadeTo(1800, 1);
+    $("#resume").delay(900).fadeTo(1800, 1);
 
 
     //HOVER FOR BUTTONS (.bordered-link)
